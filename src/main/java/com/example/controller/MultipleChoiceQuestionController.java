8f510e1876f33b457d28b0ac659b23e254bed0aa
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-27 15:31:09 +0530

    created Update API

--- src/main/java/com/example/controller/MultipleChoiceQuestionController.java
+++ src/main/java/com/example/controller/MultipleChoiceQuestionController.java
@@ -6,14 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.MultipleChoiceQuestion;
+import com.example.response.SuccessResponse;
 import com.example.service.MultipleChoiceQuestionService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -24,20 +29,41 @@ public class MultipleChoiceQuestionController {
     MultipleChoiceQuestionService multipleChoiceQuestionService;
     
     @PostMapping
-    public ResponseEntity<MultipleChoiceQuestion> savQuestion(@RequestBody MultipleChoiceQuestion question){
-        return new ResponseEntity<>(multipleChoiceQuestionService.savQuestion(question), HttpStatus.CREATED);
+    public ResponseEntity<SuccessResponse> savQuestion(@RequestBody MultipleChoiceQuestion question){
+        MultipleChoiceQuestion questionSaved = multipleChoiceQuestionService.savQuestion(question);
+        SuccessResponse successResponse = new SuccessResponse("Question created Successfully", HttpStatus.CREATED.value(), questionSaved); 
+        return new ResponseEntity<>(successResponse, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-all")
-    public ResponseEntity<List<MultipleChoiceQuestion>> getAllQuestionsData(){
+    public ResponseEntity<SuccessResponse> getAllQuestionsData(){
         List<MultipleChoiceQuestion> allQuestions = multipleChoiceQuestionService.getAllQuestionsData();
-        return new ResponseEntity<>(allQuestions, HttpStatus.OK);
+        SuccessResponse successResponse = new SuccessResponse("All Questions data retrieved successfully.", HttpStatus.OK.value(), allQuestions);
+        return new ResponseEntity<>(successResponse, HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity<MultipleChoiceQuestion> getQuestionById(@RequestParam("questionId") Integer questionId){
+    public ResponseEntity<SuccessResponse> getQuestionById(@RequestParam("questionId") Integer questionId){
         MultipleChoiceQuestion question = multipleChoiceQuestionService.getQuestionData(questionId);
-        return new ResponseEntity<>(question, HttpStatus.OK);
+        SuccessResponse successResponse = new SuccessResponse("Question found and retrieved successfully.", HttpStatus.FOUND.value(), question);
+        return new ResponseEntity<>(successResponse, HttpStatus.FOUND);
+    }
+
+    @PutMapping("/{questionId}")
+    public ResponseEntity<SuccessResponse> updateQuestion(@PathVariable("questionId") Integer questionId, @RequestBody MultipleChoiceQuestion question){
+        MultipleChoiceQuestion updatedQuestion = multipleChoiceQuestionService.updateQuestion(questionId, question);
+        SuccessResponse successResponse = new SuccessResponse("Question data updated successfully.", HttpStatus.OK.value(), updatedQuestion);
+        return new ResponseEntity<>(successResponse, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{questionId}")
+    public ResponseEntity<SuccessResponse> deleteQuestionById(@PathVariable("questionId") Integer questionId){
+        multipleChoiceQuestionService.deleteQuestion(questionId);
+        SuccessResponse successResponse = new SuccessResponse();
+        successResponse.setMessage("Question deleted successfully");
+            successResponse.setStatusCode(HttpStatus.OK.value());
+        return new ResponseEntity<>(successResponse, HttpStatus.OK);
+
     }
 
 }
