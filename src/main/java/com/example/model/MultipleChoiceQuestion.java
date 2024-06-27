bc10cb222b12ef41d02a0c1e08ef36c339a61058
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-27 11:48:24 +0530

    created MCQ model

---
+++ src/main/java/com/example/model/MultipleChoiceQuestion.java
@@ -0,0 +1,30 @@
+package com.example.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "mcq_question")
+public class MultipleChoiceQuestion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer questionId;
+    String category;
+    String optionOne;
+    String optionTwo;
+    String optionThree;
+    String optionFour;
+    Integer correctOption;
+    Integer positiveMark;
+    Integer negativeMark;
+}
