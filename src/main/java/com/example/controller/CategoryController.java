47be2396d7996e47aa2da8d44e5decd42d250eb2
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-28 17:28:41 +0530

    removed unnecessary line

--- src/main/java/com/example/controller/CategoryController.java
+++ src/main/java/com/example/controller/CategoryController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -49,7 +50,7 @@ public class CategoryController {
         return new ResponseEntity<>(successResponse, HttpStatus.FOUND);
     }
 
-    @PostMapping("/{categoryId}")
+    @PutMapping("/{categoryId}")
     public ResponseEntity<SuccessResponse> updateCategory(@PathVariable("categoryId") Integer categoryId,
             @RequestBody Category category) {
         Category updateCategory = categoryService.updateCategory(categoryId, category);
