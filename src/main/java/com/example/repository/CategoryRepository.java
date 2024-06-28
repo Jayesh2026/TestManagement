18df9d384f3381818f6d161bbee76e4e80c73bca
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-28 16:05:23 +0530

    remove unused import class

--- src/main/java/com/example/repository/CategoryRepository.java
+++ src/main/java/com/example/repository/CategoryRepository.java
@@ -4,9 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.model.Category;
-import java.util.List;
 import java.util.Optional;
-
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
