ac3fcfe26957c7fa2ce99646c8a7d6b3d443d8c8
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-28 14:10:33 +0530

    create SubCategory Interface

--- src/main/java/com/example/service/SubcategoryService.java
+++ src/main/java/com/example/service/SubcategoryService.java
@@ -1,8 +1,11 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class SubcategoryService {
+import com.example.repository.SubcategoryRepository;
+
+public interface SubcategoryService {
+
     
 }
