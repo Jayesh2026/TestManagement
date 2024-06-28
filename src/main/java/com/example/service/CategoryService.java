a697030b95e871d248d267f40f8a3d54d6711090
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-28 14:08:17 +0530

    create CategoryService Interface

--- src/main/java/com/example/service/CategoryService.java
+++ src/main/java/com/example/service/CategoryService.java
@@ -1,8 +1,18 @@
 package com.example.service;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
 
-@Service
-public class CategoryService {
+import com.example.model.Category;
+
+public interface CategoryService {
     
+    public Category createCategory(Category category);
+
+    public List<Category> getAllCategories();
+
+    public Category getCategoryByIdOrCategoryName(Category category);
+
+    public Category updateCategory(Category category);
+
+    public void deleteCategoryById(Integer categoryId);
 }
