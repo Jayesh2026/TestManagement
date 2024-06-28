1b3b4e43f85349bcd9d0f4b6f8c47940dfac745d
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-28 12:19:01 +0530

    added @Repository annotation

--- src/main/java/com/example/repository/SubcategoryRepository.java
+++ src/main/java/com/example/repository/SubcategoryRepository.java
@@ -1,9 +1,11 @@
 package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.model.Subcategory;
 
+@Repository
 public interface SubcategoryRepository extends JpaRepository<Subcategory, Integer> {
     
 }
