e72e90be6f0be3b30ec5704900ba5f311fbafa42
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-28 15:07:25 +0530

    created getCategoryByName API

--- src/main/java/com/example/controller/CategoryController.java
+++ src/main/java/com/example/controller/CategoryController.java
@@ -1,10 +1,44 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Category;
+import com.example.model.MultipleChoiceQuestion;
+import com.example.response.SuccessResponse;
+import com.example.service.CategoryService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/category")
 public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
     
+    @PostMapping
+    public ResponseEntity<SuccessResponse> createCategory(@RequestBody Category category){
+        Category savedCategory = categoryService.createCategory(category);
+        SuccessResponse successResponse = new SuccessResponse("Category created Successfully.", HttpStatus.CREATED.value(), savedCategory);
+        return new ResponseEntity<>(successResponse, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<SuccessResponse> getAllCategory(){
+        List<Category> categoryList = categoryService.getAllCategories();
+        SuccessResponse successResponse = new SuccessResponse("All Categories are retrieved successfully.", HttpStatus.OK.value(), categoryList);
+        return new ResponseEntity<>(successResponse, HttpStatus.OK);
+    }
+
+    public ResponseEntity<SuccessResponse> getCategoryByName(@RequestBody String categoryName){
+        Category category = categoryService.getCategoryByCategoryName(categoryName);
+        SuccessResponse successResponse = new SuccessResponse("All Categories are retrieved successfully.", HttpStatus.FOUND.value(), category);
+        return new ResponseEntity<>(successResponse, HttpStatus.FOUND);
+    }
+
 }
