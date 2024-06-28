9e8f6f0f265eac42b6798096a594d7d66371d8b5
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-28 15:12:03 +0530

    update repository

--- src/main/java/com/example/repository/CategoryRepository.java
+++ src/main/java/com/example/repository/CategoryRepository.java
@@ -4,9 +4,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.model.Category;
+import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
-    
+    boolean existsByCategoryId(Integer categoryId);
+
+    boolean existsByCategoryName(String categoryName);
+
+    Optional<Category> findByCategoryName(String categoryName);
+
+    void deleteByCategoryName(String categoryName);
 }
