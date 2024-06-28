6ab2b78f89966c2f577b1557d5d34df19198240c
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-28 17:29:40 +0530

    create ObjectNotFoundException

---
+++ src/main/java/com/example/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+    
+    public ObjectNotFoundException(String message){
+        super(message);
+    }
+}
