320173163070cf03f9a750f6cba2efb6175f0fc5
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-28 16:03:09 +0530

    create delete and update API

--- src/main/java/com/example/controller/CategoryController.java
+++ src/main/java/com/example/controller/CategoryController.java
@@ -3,7 +3,9 @@ package com.example.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.Category;
-import com.example.model.MultipleChoiceQuestion;
 import com.example.response.SuccessResponse;
 import com.example.service.CategoryService;
 
@@ -23,26 +24,47 @@ public class CategoryController {
 
     @Autowired
     CategoryService categoryService;
-    
+
     @PostMapping
-    public ResponseEntity<SuccessResponse> createCategory(@RequestBody Category category){
+    public ResponseEntity<SuccessResponse> createCategory(@RequestBody Category category) {
         Category savedCategory = categoryService.createCategory(category);
-        SuccessResponse successResponse = new SuccessResponse("Category created Successfully.", HttpStatus.CREATED.value(), savedCategory);
+        SuccessResponse successResponse = new SuccessResponse("Category created Successfully.",
+                HttpStatus.CREATED.value(), savedCategory);
         return new ResponseEntity<>(successResponse, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<SuccessResponse> getAllCategory(){
+    public ResponseEntity<SuccessResponse> getAllCategory() {
         List<Category> categoryList = categoryService.getAllCategories();
-        SuccessResponse successResponse = new SuccessResponse("All Categories are retrieved successfully.", HttpStatus.OK.value(), categoryList);
+        SuccessResponse successResponse = new SuccessResponse("All Categories are retrieved successfully.",
+                HttpStatus.OK.value(), categoryList);
         return new ResponseEntity<>(successResponse, HttpStatus.OK);
     }
 
     @GetMapping("/")
-    public ResponseEntity<SuccessResponse> getCategoryByName(@RequestParam("categoryName") String categoryName){
+    public ResponseEntity<SuccessResponse> getCategoryByName(@RequestParam("categoryName") String categoryName) {
         Category category = categoryService.getCategoryByCategoryName(categoryName);
-        SuccessResponse successResponse = new SuccessResponse("All Categories are retrieved successfully.", HttpStatus.FOUND.value(), category);
+        SuccessResponse successResponse = new SuccessResponse("Category is retrieved successfully.",
+                HttpStatus.FOUND.value(), category);
         return new ResponseEntity<>(successResponse, HttpStatus.FOUND);
+    }
+
+    @PostMapping("/{categoryId}")
+    public ResponseEntity<SuccessResponse> updateCategory(@PathVariable("categoryId") Integer categoryId,
+            @RequestBody Category category) {
+        Category updateCategory = categoryService.updateCategory(categoryId, category);
+        SuccessResponse successResponse = new SuccessResponse("Category updated successfully.", HttpStatus.OK.value(),
+                updateCategory);
+        return new ResponseEntity<>(successResponse, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity<SuccessResponse> deleteCategoryByName(@PathVariable("categoryId") Integer categoryId) {
+        categoryService.deleteCategoryById(categoryId);
+        SuccessResponse successResponse = new SuccessResponse();
+        successResponse.setMessage("Category deleted successfully");
+        successResponse.setStatusCode(HttpStatus.OK.value());
+        return new ResponseEntity<>(successResponse, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/service/Impl/CategoryServiceImpl.java
+++ src/main/java/com/example/service/Impl/CategoryServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.service.Impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.exception.ObjectIsNullException;
+import com.example.exception.CategoryNotFoundException;
+import com.example.exception.DuplicateCategoryException;
+import com.example.exception.IllegalArgumentException;
 import com.example.model.Category;
 import com.example.repository.CategoryRepository;
 import com.example.service.CategoryService;
@@ -18,30 +21,69 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public Category createCategory(Category category) {
-        if(category == null){
-            throw new ObjectIsNullException("Category should not be null");
+        if (category == null) {
+            throw new IllegalArgumentException("Category should not be null");
         }
+
+        if (categoryRepository.existsByCategoryName(category.getCategoryName())) {
+            throw new DuplicateCategoryException("Category with name '" + category.getCategoryName() + "' already exists");
+        }
+
         return categoryRepository.save(category);
     }
 
     @Override
     public List<Category> getAllCategories() {
-        return null;
+        List<Category> categories = categoryRepository.findAll();
+        if (categories == null || categories.isEmpty()) {
+            throw new CategoryNotFoundException("Unable to retrieved all categories, Not found any question.");
+        }
+        return categories;
     }
 
     @Override
-    public Category getCategoryByIdOrCategoryName(Category category) {
-        return null;
+    public Category getCategoryByCategoryName(String categoryName) {
+        Optional<Category> categoryData = categoryRepository.findByCategoryName(categoryName);
+        if(!categoryData.isPresent()){
+            throw new CategoryNotFoundException("Name of '" + categoryName + "' category is not present.");
+        }
+        return categoryData.get();
     }
 
     @Override
-    public Category updateCategory(Category category) {
-        return null;
+    public Category updateCategory(Integer categoryId, Category updatedCategory) {
+        Optional<Category> exitingCategory = categoryRepository.findById(categoryId);
+        if(!exitingCategory.isPresent()){
+            throw new CategoryNotFoundException("Category with name " + categoryId + " not found");
+        }
+
+        Category existingCategoryData = exitingCategory.get();
+
+        if (!existingCategoryData.getCategoryName().equals(updatedCategory.getCategoryName())) {
+            Optional<Category> categoryByNameOptional = categoryRepository.findByCategoryName(updatedCategory.getCategoryName());
+            if (categoryByNameOptional.isPresent()) {
+                throw new IllegalArgumentException("Category with name '" + updatedCategory.getCategoryName() + "' already exists");
+            }
+        }
+
+        // Update fields conditionally
+        if (updatedCategory.getCategoryName() != null) {
+            existingCategoryData.setCategoryName(updatedCategory.getCategoryName());
+        }
+        if (updatedCategory.getCategoryDescription() != null) {
+            existingCategoryData.setCategoryDescription(updatedCategory.getCategoryDescription());
+        }
+
+        return categoryRepository.save(existingCategoryData);
     }
 
     @Override
     public void deleteCategoryById(Integer categoryId) {
-        
+        Optional<Category> optionalCategory = categoryRepository.findById(categoryId);
+        if (optionalCategory.isPresent()) {
+            categoryRepository.deleteById(categoryId);
+        } else {
+            throw new CategoryNotFoundException("Category with Id " + categoryId + " not found");
+        }
     }
-    
 }
