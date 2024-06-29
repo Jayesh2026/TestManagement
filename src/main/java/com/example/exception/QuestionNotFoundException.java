8fd3b80f956954bbf2497d952698c2ec7df2af89
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-30 01:12:55 +0530

    global exception handling

--- src/main/java/com/example/exception/QuestionNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.exception;
-
-public class QuestionNotFoundException extends RuntimeException{
-    
-    public QuestionNotFoundException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/example/exception/TestManagementGlobalException.java
+++ src/main/java/com/example/exception/TestManagementGlobalException.java
@@ -8,11 +8,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import com.example.response.ErrorResponse;
 
 @ControllerAdvice
-public class MultipleChoiceQuestionGlobalException {
+public class TestManagementGlobalException {
     
-    @ExceptionHandler(QuestionNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleQuestionNotFoundException(QuestionNotFoundException questionException){
-        ErrorResponse errorResponse = new ErrorResponse(questionException.getMessage(), HttpStatus.NOT_FOUND.value());
+    @ExceptionHandler(DuplicateDataException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateDataException(DuplicateDataException exception){
+        ErrorResponse errorResponse = new ErrorResponse(exception.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleDataNotFoundException(DataNotFoundException exception){
+        ErrorResponse errorResponse = new ErrorResponse(exception.getMessage(), HttpStatus.NOT_FOUND.value());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
@@ -21,5 +27,4 @@ public class MultipleChoiceQuestionGlobalException {
         ErrorResponse errorResponse = new ErrorResponse(illegalArgException.getMessage(), HttpStatus.BAD_REQUEST.value());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
-
 }
