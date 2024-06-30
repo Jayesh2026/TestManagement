c25166874e9b3a402b8ebef8cd711cb70621abb3
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-30 15:15:41 +0530

    positive test cases

--- src/test/java/com/example/service/SubCategoryServiceTest.java
+++ src/test/java/com/example/service/SubCategoryServiceTest.java
@@ -17,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.example.model.Category;
 import com.example.model.SubCategory;
+import com.example.repository.CategoryRepository;
 import com.example.repository.SubCategoryRepository;
 import com.example.service.Impl.SubCategoryServiceImpl;
 
@@ -25,6 +26,9 @@ public class SubCategoryServiceTest {
 
     @Mock
     SubCategoryRepository subCategoryRepository;
+
+    @Mock
+    CategoryRepository categoryRepository;
 
     @InjectMocks
     SubCategoryServiceImpl subCategoryServiceImpl;
@@ -36,14 +40,21 @@ public class SubCategoryServiceTest {
 
     @Test
     public void testSaveSubCategory() {
-        SubCategory actualSubCategory = new SubCategory(1, "SubCategory_1", "SubCategory Details",
-                new Category(1, "Category_1", "Category details"));
+        Category category = new Category(1, "Category_1", "Category details");
+        SubCategory subCategory = new SubCategory(1, "SubCategory_1", "SubCategory Details", category);
 
-        when(subCategoryRepository.save(actualSubCategory)).thenReturn(actualSubCategory);
-        SubCategory expectSubCategory = subCategoryServiceImpl.saveSubCategory(actualSubCategory);
+        when(categoryRepository.findByCategoryIdAndCategoryNameIgnoreCase(category.getCategoryId(), category.getCategoryName())).thenReturn(Optional.of(category));
+        when(subCategoryRepository.findBySubcategoryNameIgnoreCaseAndCategory(subCategory.getSubcategoryName(), category))
+                                                                .thenReturn(Optional.empty()); // No existing subcategory with the same name
+        when(subCategoryRepository.save(subCategory)).thenReturn(subCategory);
 
-        verify(subCategoryRepository, times(1)).save(actualSubCategory);
-        assertEquals(actualSubCategory, expectSubCategory);
+        SubCategory savedSubCategory = subCategoryServiceImpl.saveSubCategory(subCategory);
+
+        verify(categoryRepository, times(1)).findByCategoryIdAndCategoryNameIgnoreCase(category.getCategoryId(), category.getCategoryName());
+        verify(subCategoryRepository, times(1)).findBySubcategoryNameIgnoreCaseAndCategory(subCategory.getSubcategoryName(), category);
+        verify(subCategoryRepository, times(1)).save(subCategory);
+
+        assertEquals(subCategory, savedSubCategory);
     }
 
     @Test
@@ -95,13 +106,13 @@ public class SubCategoryServiceTest {
     @Test
     public void testDeleteSubCategory() {
         Integer subCategoryId = 1;
-        SubCategory existingSubCategory = new SubCategory(subCategoryId, "SubCategory_1", "SubCategory Details",
-                new Category(1, "Category_1", "Category details"));
+        SubCategory subCategory = new SubCategory(subCategoryId, "SubCategory_1", "SubCategory Details", new Category());
 
-        when(subCategoryRepository.findById(subCategoryId)).thenReturn(Optional.of(existingSubCategory));
+        when(subCategoryRepository.findBySubcategoryId(subCategoryId)).thenReturn(Optional.of(subCategory));
+        doNothing().when(subCategoryRepository).deleteById(subCategoryId);
         subCategoryServiceImpl.deleteSubCategory(subCategoryId);
 
-        verify(subCategoryRepository, times(1)).findById(subCategoryId);
+        verify(subCategoryRepository, times(1)).findBySubcategoryId(subCategoryId);
         verify(subCategoryRepository, times(1)).deleteById(subCategoryId);
     }
 }
