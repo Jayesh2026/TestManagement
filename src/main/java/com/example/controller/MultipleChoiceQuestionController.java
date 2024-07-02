b75062e1085e3b1004047875deba78411c1fa889
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-07-03 01:58:38 +0530

    Create API for save bulk questions from excel file

--- src/main/java/com/example/controller/MultipleChoiceQuestionController.java
+++ src/main/java/com/example/controller/MultipleChoiceQuestionController.java
@@ -2,6 +2,8 @@ package com.example.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.model.MultipleChoiceQuestion;
 import com.example.response.SuccessResponse;
@@ -19,49 +23,68 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 @RestController
 @RequestMapping("/api/questions")
 public class MultipleChoiceQuestionController {
 
+    private static final Logger logger = LoggerFactory.getLogger(MultipleChoiceQuestionController.class);
+
     @Autowired
     MultipleChoiceQuestionService multipleChoiceQuestionService;
-    
+
     @PostMapping
-    public ResponseEntity<SuccessResponse> saveQuestion(@RequestBody MultipleChoiceQuestion question){
+    public ResponseEntity<SuccessResponse> saveQuestion(@RequestBody MultipleChoiceQuestion question) {
         MultipleChoiceQuestion questionSaved = multipleChoiceQuestionService.saveQuestion(question);
-        SuccessResponse successResponse = new SuccessResponse("Question created Successfully", HttpStatus.CREATED.value(), questionSaved); 
+        SuccessResponse successResponse = new SuccessResponse("Question created Successfully",
+                HttpStatus.CREATED.value(), questionSaved);
         return new ResponseEntity<>(successResponse, HttpStatus.CREATED);
     }
 
     @GetMapping("/")
-    public ResponseEntity<SuccessResponse> getAllQuestionsData(){
+    public ResponseEntity<SuccessResponse> getAllQuestionsData() {
         List<MultipleChoiceQuestion> allQuestions = multipleChoiceQuestionService.getAllQuestionsData();
-        SuccessResponse successResponse = new SuccessResponse("All Questions data retrieved successfully.", HttpStatus.OK.value(), allQuestions);
+        SuccessResponse successResponse = new SuccessResponse("All Questions data retrieved successfully.",
+                HttpStatus.OK.value(), allQuestions);
         return new ResponseEntity<>(successResponse, HttpStatus.OK);
     }
 
     @GetMapping("/{questionId}")
-    public ResponseEntity<SuccessResponse> getQuestionById(@PathVariable("questionId") Integer questionId){
+    public ResponseEntity<SuccessResponse> getQuestionById(@PathVariable("questionId") Integer questionId) {
         MultipleChoiceQuestion question = multipleChoiceQuestionService.getQuestionData(questionId);
-        SuccessResponse successResponse = new SuccessResponse("Question found and retrieved successfully.", HttpStatus.FOUND.value(), question);
+        SuccessResponse successResponse = new SuccessResponse("Question found and retrieved successfully.",
+                HttpStatus.FOUND.value(), question);
         return new ResponseEntity<>(successResponse, HttpStatus.FOUND);
     }
 
     @PutMapping("/{questionId}")
-    public ResponseEntity<SuccessResponse> updateQuestion(@PathVariable("questionId") Integer questionId, @RequestBody MultipleChoiceQuestion question){
+    public ResponseEntity<SuccessResponse> updateQuestion(@PathVariable("questionId") Integer questionId,
+            @RequestBody MultipleChoiceQuestion question) {
         MultipleChoiceQuestion updatedQuestion = multipleChoiceQuestionService.updateQuestion(questionId, question);
-        SuccessResponse successResponse = new SuccessResponse("Question data updated successfully.", HttpStatus.OK.value(), updatedQuestion);
+        SuccessResponse successResponse = new SuccessResponse("Question data updated successfully.",
+                HttpStatus.OK.value(), updatedQuestion);
         return new ResponseEntity<>(successResponse, HttpStatus.OK);
     }
 
     @DeleteMapping("/{questionId}")
-    public ResponseEntity<SuccessResponse> deleteQuestionById(@PathVariable("questionId") Integer questionId){
+    public ResponseEntity<SuccessResponse> deleteQuestionById(@PathVariable("questionId") Integer questionId) {
         multipleChoiceQuestionService.deleteQuestion(questionId);
         SuccessResponse successResponse = new SuccessResponse();
         successResponse.setMessage("Question deleted successfully");
-            successResponse.setStatusCode(HttpStatus.OK.value());
+        successResponse.setStatusCode(HttpStatus.OK.value());
         return new ResponseEntity<>(successResponse, HttpStatus.OK);
+    }
+
+    @PostMapping("/saveBulkQuestions")
+    public ResponseEntity<SuccessResponse> uploadBulkQuestions(@RequestParam("file") MultipartFile file) {
+        try {
+            multipleChoiceQuestionService.uploadBulkQuestionsFromExcelFile(file);
+            SuccessResponse successResponse = new SuccessResponse("Question Data Uploaded Successfully.", HttpStatus.OK.value(), null);
+            return new ResponseEntity<SuccessResponse>(successResponse, HttpStatus.OK);
+        } catch (Exception exception) {
+            logger.error(exception.getMessage(), exception);
+            SuccessResponse successResponse = new SuccessResponse("Unable to Upload question: "+ exception.getMessage(), 400, null);
+            return new ResponseEntity<SuccessResponse>(successResponse, HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
--- src/main/java/com/example/service/Impl/MultipleChoiceQuestionServiceImpl.java
+++ src/main/java/com/example/service/Impl/MultipleChoiceQuestionServiceImpl.java
@@ -1,15 +1,26 @@
 package com.example.service.Impl;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.exception.DataNotFoundException;
+import com.example.model.Category;
 import com.example.model.MultipleChoiceQuestion;
+import com.example.model.SubCategory;
+import com.example.repository.CategoryRepository;
 import com.example.repository.MultipleChoiceQuestionRepository;
 import com.example.repository.SubCategoryRepository;
 import com.example.service.MultipleChoiceQuestionService;
@@ -24,6 +35,9 @@ public class MultipleChoiceQuestionServiceImpl implements MultipleChoiceQuestion
 
     @Autowired
     SubCategoryRepository subCategoryRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
 
     @Override
     public MultipleChoiceQuestion saveQuestion(MultipleChoiceQuestion question) {
@@ -51,7 +65,7 @@ public class MultipleChoiceQuestionServiceImpl implements MultipleChoiceQuestion
     public MultipleChoiceQuestion getQuestionData(Integer questionId) {
         Optional<MultipleChoiceQuestion> questionData = multipleChoiceQuestionRepository.findById(questionId);
 
-        if(!questionData.isPresent()){
+        if (!questionData.isPresent()) {
             throw new DataNotFoundException("This question is not present.");
         }
         return questionData.get();
@@ -103,10 +117,108 @@ public class MultipleChoiceQuestionServiceImpl implements MultipleChoiceQuestion
     @Override
     public void deleteQuestion(Integer questionId) {
         Optional<MultipleChoiceQuestion> question = multipleChoiceQuestionRepository.findById(questionId);
-        if(!question.isPresent()){
+        if (!question.isPresent()) {
             throw new DataNotFoundException("This question Id is not found.");
         }
-
         multipleChoiceQuestionRepository.deleteById(questionId);
     }
+
+    @Override
+    public void uploadBulkQuestionsFromExcelFile(MultipartFile multipartFile) {
+        List<MultipleChoiceQuestion> questionsList = new ArrayList<>();
+        
+        try (Workbook workbook = WorkbookFactory.create(multipartFile.getInputStream())) {
+            Sheet sheet = workbook.getSheetAt(0);
+
+            for (int rowIndex = sheet.getFirstRowNum()+1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+                Row currentRow = sheet.getRow(rowIndex);
+                if (currentRow == null) {
+                    continue; // Skip if row is empty
+                }
+
+                MultipleChoiceQuestion mcqQuestion = new MultipleChoiceQuestion();
+                Category category = null;
+                SubCategory subcategory = null;
+                boolean skipRow = false;
+
+                for (int columnIndex = currentRow.getFirstCellNum(); columnIndex <= currentRow.getLastCellNum(); columnIndex++) {
+                    Cell currentCell = currentRow.getCell(columnIndex);
+
+                    switch (columnIndex) {
+                        case 1:
+                            String categoryName = currentCell.getStringCellValue();
+                            category = categoryRepository.findByCategoryNameIgnoreCase(categoryName)
+                                    .orElse(null);
+                            if (category == null) {
+                                skipRow = true;
+                                logger.warn("Category not found for row {}: {}", rowIndex, categoryName);
+                            }
+                            break;
+                        case 2:
+                            if (!skipRow && category != null) {
+                                String subcategoryName = currentCell.getStringCellValue();
+                                subcategory = subCategoryRepository
+                                        .findBySubcategoryNameIgnoreCaseAndCategory(subcategoryName, category)
+                                        .orElse(null);
+                                if (subcategory == null) {
+                                    skipRow = true;
+                                    logger.warn("Subcategory not found for row {}: {}", rowIndex, subcategoryName);
+                                }
+                            }
+                            break;
+                        case 3:
+                            if (!skipRow) {
+                                mcqQuestion.setQuestion(currentCell.getStringCellValue());
+                            }
+                            break;
+                        case 4:
+                            if (!skipRow) {
+                                mcqQuestion.setOptionOne(currentCell.getStringCellValue());
+                            }
+                            break;
+                        case 5:
+                            if (!skipRow) {
+                                mcqQuestion.setOptionTwo(currentCell.getStringCellValue());
+                            }
+                            break;
+                        case 6:
+                            if (!skipRow) {
+                                mcqQuestion.setOptionThree(currentCell.getStringCellValue());
+                            }
+                            break;
+                        case 7:
+                            if (!skipRow) {
+                                mcqQuestion.setOptionFour(currentCell.getStringCellValue());
+                            }
+                            break;
+                        case 8:
+                            if (!skipRow) {
+                                mcqQuestion.setCorrectOption(currentCell.getStringCellValue());
+                            }
+                            break;
+                        case 9:
+                            if (!skipRow) {
+                                mcqQuestion.setPositiveMark(currentCell.toString());
+                            }
+                            break;
+                        case 10:
+                            if (!skipRow) {
+                                mcqQuestion.setNegativeMark(currentCell.toString());
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                if (!skipRow && subcategory != null) {
+                    mcqQuestion.setSubcategory(subcategory);
+                    questionsList.add(mcqQuestion);
+                }
+            }
+            multipleChoiceQuestionRepository.saveAll(questionsList);
+        }catch (IOException e){
+            logger.error("Error failed to read or upload file {}: {}", multipartFile.getOriginalFilename(), e.getMessage());
+        }
+    }
+
 }
